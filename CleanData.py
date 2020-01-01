d789f1388becc55f246b546dec9a8e1a3968705b
zhiyuanpeng <zpeng@scu.edu>
2019-12-31 18:36:55 -0800

    remove the genres with f < 100

--- CleanData.py
+++ CleanData.py
@@ -44,23 +44,29 @@ genres = []
 for i in movies['genre']:
     genres.append(list(json.loads(i).values()))
 
-# add to 'movies' dataframe
-movies['genre_new'] = genres
-
-movies_new = movies[~(movies['genre_new'].str.len() == 0)]
+# get statistical info of genres
 all_genres = sum(genres, [])
 unique_genres = list(set(all_genres))
-
 all_genres = nltk.FreqDist(all_genres)
 # create dataframe
 all_genres_df = pd.DataFrame({'Genre': list(all_genres.keys()),
                               'Count': list(all_genres.values())})
-genres_selected = all_genres_df[all_genres_df['Count'] >= 400]
+genres_selected = all_genres_df[all_genres_df['Count'] >= 100]
+# delete the genres not contained in the genres_selected
+frequent_genres = list(genres_selected.sort_values(by=['Count'], ascending=False)['Genre'])
+for row in tqdm(genres):
+    for index in range(len(row) - 1):
+        if row[index] not in frequent_genres:
+            row.remove(row[index])
+movies['genre_new'] = genres
+# delete the all 0 genre
+movies_new = movies[~(movies['genre_new'].str.len() == 0)]
+#
 # g = all_genres_df.nlargest(columns="Count", n=50)
-# plt.figure(figsize=(12, 15))
-ax = sns.barplot(data=genres_selected, x="Count", y="Genre")
-ax.set(ylabel='Count')
-plt.show()
+# plt.figure(figsize=(35, 45))
+# ax = sns.barplot(data=genres_selected, x="Count", y="Genre")
+# ax.set(ylabel='Count')
+# plt.show()
 print("done")
 
 
