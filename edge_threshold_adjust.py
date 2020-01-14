e279670ee332fcb9f3831eb6872a05c75d36683f
zhiyuanpeng <zpeng@scu.edu>
2020-01-13 16:42:30 -0800

    update files

--- edge_threshold_adjust.py
+++ edge_threshold_adjust.py
@@ -9,7 +9,7 @@ def x_write(list_name, list_to_file_name):
 
 def read_to_list_threshold(file_name, threshold):
     list_threshold = []
-    with open("./data/split_test/" + file_name + "_predict_1000.txt", "r") as f:
+    with open("./data/split_test/" + file_name + "_predict_2000.txt", "r") as f:
         without_round = f.readlines()
         for value in without_round:
             if float(value) > threshold:
@@ -21,12 +21,18 @@ def read_to_list_threshold(file_name, threshold):
 
 def round_with_threshold(file_name, threshold):
     round_list = read_to_list_threshold(file_name, threshold)
-    x_write(round_list, "./data/split_test/" + file_name + "_predict_round_" + str(round(threshold, 2)) + "_1000.txt")
+    x_write(round_list, "./data/split_test/" + file_name + "_predict_round_" + str(threshold) + "_2000.txt")
 
 
 def adjust_threshold(file_dire, threshold):
-    predict = np.loadtxt("./data/split_test/" + file_dire + "_predict_575.txt")
-    label = np.loadtxt("./data/split_test/" + file_dire + "_truth_575.txt", dtype=int)
+    """
+    if the predict value > threshold, value = 1
+    :param file_dire:
+    :param threshold:
+    :return: the predict_round and right_sum
+    """
+    predict = np.loadtxt("./data/split_test/" + file_dire + "_predict_878.txt")
+    label = np.loadtxt("./data/split_test/" + file_dire + "_truth_878.txt", dtype=int)
     right_sum = 0
     predict_round = []
     for i in range(predict.shape[0]):
@@ -37,24 +43,43 @@ def adjust_threshold(file_dire, threshold):
         predict_round.append(after_round)
         if after_round == label[i, ]:
             right_sum += 1
-    print("with threshold " + str(round(threshold, 2)) + ", the accuracy is " + str(right_sum/predict.shape[0]))
-    x_write(predict_round, "./data/store/" + file_dire + "/" + file_dire + "_predict_round_" + str(round(threshold, 2)) + "_575.txt")
+    return predict_round, right_sum/predict.shape[0]
 
 
 def optimal_search(file_name):
+    """
+    find the maximum right_sum and the corresponding threshold, predict result
+    :param file_name:
+    :return:
+    """
     threshold = 0.5
+    predict_list = []
+    accuracy_list = []
+    threshold_list = []
     for index in range(50):
         threshold += 0.01
-        adjust_threshold(file_name, threshold)
+        predict_round, accuracy = adjust_threshold(file_name, threshold)
+        predict_list.append(predict_round)
+        accuracy_list.append(accuracy)
+        threshold_list.append(threshold)
+    max_index = accuracy_list.index(np.max(accuracy_list))
+    return predict_list[max_index], accuracy_list[max_index], round(threshold_list[max_index], 2)
 
 
 def main():
-    #optimal_search("l1_l2")
-    #round_with_threshold("l0_l1", 0.92)
-    round_with_threshold("l0_l3", 0.98)
-    round_with_threshold("l0_l4", 0.54)
-    round_with_threshold("l0_l5", 0.86)
-    round_with_threshold("l1_l2", 0.91)
+    edge_list = [(0, 3), (0, 5), (0, 6), (0, 9), (0, 10), (0, 12), (0, 14), (1, 4), (1, 7), (1, 8), (1, 11), (1, 13),
+                 (2, 3), (2, 4)]
+    optimal_file = "data/optimal_result.txt"
+    for edge in edge_list:
+        # get the optimal
+        edge_name = "l" + str(edge[0]) + "_" + "l" + str(edge[1])
+        opt_predict, opt_accracy, opt_threshold = optimal_search(edge_name)
+        # use the optimal threshold to deal the left 2000 data
+        round_with_threshold(edge_name, opt_threshold)
+        with open(optimal_file, "a+") as opt:
+            opt.write(edge_name + " with optimal threshold " + str(opt_threshold) + ", the accuracy is " + str(opt_accracy) + "\n")
+        x_write(opt_predict, "./data/store/" + edge_name + "/" + edge_name + "_predict_round_"
+                + str(opt_threshold) + "_878.txt")
 
 
 if __name__ == '__main__':
--- enumeration.py
+++ enumeration.py
@@ -1,5 +1,7 @@
 import numpy as np
 import math
+from tqdm import tqdm
+
 
 
 def bit_to_list(t, n):
@@ -30,7 +32,7 @@ def get_candidate(list_length):
     return candidate
 
 
-def transfer_label(trans_value,):
+def transfer_label(trans_value):
     """
     trans_value is the possibility of 1 1, we need to get
        0 1
@@ -46,38 +48,33 @@ def transfer_label(trans_value,):
 
 
 #  combine the result of each binary classifier
-def get_node_matrix(node_list):
+def get_edge_matrix(link_list):
     # get the matrix size
     path = "./data/split_test/"
-    example = list(np.loadtxt(path + node_list[0]))
-    node_matrix = np.zeros((len(example), len(node_list)))
-    for index in range(len(node_list)):
-        node_column = list(np.loadtxt(path + node_list[index]))
-        node_matrix[:, index] = node_column
-    return node_matrix
+    example = list(np.loadtxt(path + link_list[0]))
+    edge_matrix = np.zeros((len(example), len(link_list)))
+    for index in range(len(link_list)):
+        edge_column = list(np.loadtxt(path + link_list[index]))
+        edge_matrix[:, index] = edge_column
+    return edge_matrix
 
 
-def get_transfer_matrix(link_list):
+def get_transfer_matrix(link_list, edge_list):
     transfer_list = []
     # convert the file to matrix
-    transfer_data = get_node_matrix(link_list)
+    transfer_data = get_edge_matrix(link_list)
     for line in transfer_data:
         transfer_matrix = np.zeros((len(line) + 1, len(line) + 1))
-        transfer_matrix[0, 1] = line[0]
-        transfer_matrix[0, 3] = line[1]
-        transfer_matrix[0, 4] = line[2]
-        transfer_matrix[0, 5] = line[3]
-        transfer_matrix[1, 2] = line[4]
-        #
-        transfer_matrix[1, 0] = line[0]
-        transfer_matrix[3, 0] = line[1]
-        transfer_matrix[4, 0] = line[2]
-        transfer_matrix[5, 0] = line[3]
-        transfer_matrix[2, 1] = line[4]
+        line_index = 0
+        for edge in edge_list:
+            transfer_matrix[edge[0], edge[1]] = line[line_index]
+            transfer_matrix[edge[1], edge[0]] = line[line_index]
+            line_index += 1
         transfer_list.append(transfer_matrix.copy())
     return transfer_list
 
-def overall_optimal(status_list, transfer_matrix, lambda_value):
+
+def overall_optimal(status_list, transfer_matrix, edge_list):
     score_list = []
     all_possible = get_candidate(len(status_list))
     for one_possible in all_possible:
@@ -86,24 +83,9 @@ def overall_optimal(status_list, transfer_matrix, lambda_value):
             logpossibility = status_list[index] if one_possible[index] == 1 else np.log(1 - math.exp(status_list[index]))
             score += logpossibility
         # add the transfer possibility
-        # for 0 1
-        label_trans_matrix = transfer_label(transfer_matrix[0, 1])
-        score += label_trans_matrix[one_possible[0], one_possible[1]]
-        # for 1 2
-        label_trans_matrix = transfer_label(transfer_matrix[1, 2])
-        score += label_trans_matrix[one_possible[1], one_possible[2]]
-        # for 0 3
-        label_trans_matrix = transfer_label(transfer_matrix[0, 3])
-        score += label_trans_matrix[one_possible[0], one_possible[3]]
-        # for 0 4
-        label_trans_matrix = transfer_label(transfer_matrix[0, 4])
-        score += label_trans_matrix[one_possible[0], one_possible[4]]
-        # for 0 5
-        label_trans_matrix = transfer_label(transfer_matrix[0, 5])
-        score += label_trans_matrix[one_possible[0], one_possible[5]]
-        #
-        penalty = lambda_value*np.sum(one_possible)
-        score += lambda_value*np.sum(one_possible)
+        for edge in edge_list:
+            label_trans_matrix = transfer_label(transfer_matrix[edge[0], edge[1]])
+            score += label_trans_matrix[one_possible[edge[0]], one_possible[edge[1]]]
         score_list.append(score)
     max_score = max(score_list)
     max_index = score_list.index(max_score)
@@ -111,20 +93,19 @@ def overall_optimal(status_list, transfer_matrix, lambda_value):
     return max_score, max_label
 
 
-def exhust_search(filename, lambda_value):
-    node_path_list = ["l0_predict_575.txt", "l1_predict_575.txt", "l2_predict_575.txt", "l3_predict_575.txt",
-                      "l4_predict_575.txt", "l5_predict_575.txt"]
-
-    link_path_list = ["l0_l1_predict_round_0.92_1000.txt", "l0_l3_predict_round_0.98_1000.txt", "l0_l4_predict_round_0.54_1000.txt",
-                      "l0_l5_predict_round_0.86_1000.txt", "l1_l2_predict_round_0.91_1000.txt"]
-    # link_path_list = ["l0_l1_truth_575.txt", "l0_l3_truth_575.txt", "l0_l4_truth_575.txt",
-    #                   "l0_l5_truth_575.txt", "l1_l2_truth_575.txt"]
-    # node_matrix = v.get_node_matrix(node_path_list)
-    node_matrix = np.loadtxt("data/store/original/original_predict_1000.txt")
-    transfer_list = get_transfer_matrix(link_path_list)
+def exhust_search(filename, edge_list):
+    link_path_list = ["l0_l3_predict_round_0.75_2000.txt", "l0_l5_predict_round_0.86_2000.txt",
+                      "l0_l6_predict_round_0.71_2000.txt", "l0_l9_predict_round_0.74_2000.txt",
+                      "l0_l10_predict_round_0.97_2000.txt", "l0_l12_predict_round_0.96_2000.txt",
+                      "l0_l14_predict_round_0.6_2000.txt", "l1_l4_predict_round_0.89_2000.txt",
+                      "l1_l7_predict_round_0.89_2000.txt", "l1_l8_predict_round_0.82_2000.txt",
+                      "l1_l11_predict_round_0.92_2000.txt", "l1_l13_predict_round_0.69_2000.txt",
+                      "l2_l3_predict_round_0.85_2000.txt", "l2_l4_predict_round_0.94_2000.txt"]
+    node_matrix = np.loadtxt("data/store/original/original_predict_2000.txt")
+    transfer_list = get_transfer_matrix(link_path_list, edge_list)
     with open("data/enumeration/" + filename, "a+") as vf:
         iteration = 0
-        for index in range(node_matrix.shape[0]):
+        for index in tqdm(range(node_matrix.shape[0])):
             node_matrix_log = np.zeros_like(node_matrix[index, :])
             for i in range(len(node_matrix[index, :])):
                 node_matrix_log[i, ] = np.log(node_matrix[index, i])
@@ -135,7 +116,7 @@ def exhust_search(filename, lambda_value):
                         transfer_list_log[i, j] = 0
                     else:
                         transfer_list_log[i, j] = -1000
-            max_score, best_label = overall_optimal(node_matrix_log, transfer_list_log, lambda_value)
+            max_score, best_label = overall_optimal(node_matrix_log, transfer_list_log, edge_list)
             s = ""
             for i in range(len(best_label)):
                 if i != len(best_label) - 1:
@@ -152,14 +133,9 @@ def exhust_search(filename, lambda_value):
 
 
 def main():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("file_name", type=str, help="file name of the result file")
-    # parser.add_argument("lambda_value", type=float, help="lambda * #of 1s")
-    # args = parser.parse_args()
-    # args.file_name = "exhaustion_print_575_0.3.txt"
-    # args.lambda_value = 0.3
-    # exhust_search(args.file_name, args.lambda_value)
-    exhust_search("original_edge_predict_b_differ_0_1000.txt", 0)
+    edge_list = [(0, 3), (0, 5), (0, 6), (0, 9), (0, 10), (0, 12), (0, 14), (1, 4), (1, 7), (1, 8), (1, 11), (1, 13),
+                 (2, 3), (2, 4)]
+    exhust_search("original_edge_predict_b_differ_0_2000.txt", edge_list)
 
 
 if __name__ == '__main__':
